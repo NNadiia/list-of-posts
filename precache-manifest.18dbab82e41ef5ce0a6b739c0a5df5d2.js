ca371d753fb0eb42c0664ff08d8a2ef055498149
nadiianazarchuk@gmail.com <nadiianazarchuk@gmail.com>
2019-07-01 15:05:59 +0300

    Updates

---
+++ precache-manifest.18dbab82e41ef5ce0a6b739c0a5df5d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc1f3590cfd40203f7c743d11411bcd",
+    "url": "/list-of-posts/index.html"
+  },
+  {
+    "revision": "22a7688317482b6afbc8",
+    "url": "/list-of-posts/static/css/main.8ef63b32.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/list-of-posts/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "22a7688317482b6afbc8",
+    "url": "/list-of-posts/static/js/main.0dadbcd0.chunk.js"
+  },
+  {
+    "revision": "84c2f3606d9377cebecc",
+    "url": "/list-of-posts/static/js/runtime~main.9377e6ff.js"
+  }
+]);
