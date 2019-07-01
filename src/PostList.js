4848abfe041a309d5b34b7a1dc0f752c6493e425
nadiianazarchuk@gmail.com <nadiianazarchuk@gmail.com>
2019-07-01 17:13:42 +0300

    Post item was created

--- src/PostList.js
+++ src/PostList.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+function Post(props) {
+    return (
+        <li>
+            {props.post.title}
+        </li>
+    )
+}
+
+
 function Postlist(props) {
     return (
         <ul className="Postlist">
             {props.posts.map(post => (
-                <li key={post.id}>
-                    {post.title}
-                </li>))}
+                <Post key={post.id} post={post}/>))}
         </ul>
     )
 }
