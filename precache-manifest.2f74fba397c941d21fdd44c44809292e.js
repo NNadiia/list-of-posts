ec69e14c3c7292ca6125e3ca42325d0fb659601f
nadiianazarchuk@gmail.com <nadiianazarchuk@gmail.com>
2019-07-19 02:19:54 +0300

    Updates

---
+++ precache-manifest.2f74fba397c941d21fdd44c44809292e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a08b267db64006ee0996e8d40fe5e77a",
+    "url": "/mate-academy/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "f0613ee3aac24cff4102",
+    "url": "/mate-academy/react_dynamic-list-of-posts/static/css/main.51453ab8.chunk.css"
+  },
+  {
+    "revision": "522c2e1b3f687a3c6fcb",
+    "url": "/mate-academy/react_dynamic-list-of-posts/static/js/2.3700d4c4.chunk.js"
+  },
+  {
+    "revision": "f0613ee3aac24cff4102",
+    "url": "/mate-academy/react_dynamic-list-of-posts/static/js/main.d478cd7d.chunk.js"
+  },
+  {
+    "revision": "7deb7ccbc6ee13d05678",
+    "url": "/mate-academy/react_dynamic-list-of-posts/static/js/runtime~main.0c07e2d9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/list-of-posts/precache-manifest.ec43d8f43d19e069fb527de80cc927c9.js"
+  "/mate-academy/react_dynamic-list-of-posts/precache-manifest.2f74fba397c941d21fdd44c44809292e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/list-of-posts/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mate-academy/react_dynamic-list-of-posts/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
